64d32b1403f20a5bd0d9234f439c51f5da8a6477
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-27 08:03:17 +0700

    New update

---
+++ VD_intent/app/src/main/java/tiil/edu/vd_intent/MainActivity.java
@@ -0,0 +1,39 @@
+package tiil.edu.vd_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewSo2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        textViewSo2 = findViewById(R.id.txt2);
+        textViewSo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChuyenManHinh();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    void ChuyenManHinh(){
+        Intent iManHinh2 = new Intent(this, ManHinhSo2.class);
+        startActivity(iManHinh2);
+    }
+}
